072b257536ffce49b4882562d5e09b894d200c20
Егор <eger1393@gmail.com>
2020-06-13 18:32:12 +0300

    feat(architecture): Добавил проекты для микросервисов

---
+++ Service.TpLinkApi/Startup.cs
@@ -0,0 +1,70 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using TpLinkApi.Implementation;
+using TpLinkApi.Implementation.HttpClients;
+
+namespace Service.TpLinkApi
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1",
+					new OpenApiInfo
+					{
+						Title = "API",
+						Version = "v1"
+					}
+				);
+
+				var filePath = Path.Combine(System.AppContext.BaseDirectory, "Service.TpLinkApi.xml");
+				c.IncludeXmlComments(filePath);
+			});
+
+			services.AddHttpClient<ITpLinkHttpClient, TpLinkHttpClient>();
+			services.AddScoped<ITpLink, TpLink>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+			app.UseSwagger();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+			});
+		}
+	}
+}
